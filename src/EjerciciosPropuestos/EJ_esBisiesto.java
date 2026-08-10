ea94de915d3525303aa549080f5c1a538393e7a2
Ian <ianfrancomiguez@gmail.com>
2026-08-10 20:45:49 -0300

    Ejercicio es Año Bisiesto

--- src/EjerciciosPropuestos/EJ_esBisiesto.java
+++ src/EjerciciosPropuestos/EJ_esBisiesto.java
@@ -1,4 +1,17 @@
 package EjerciciosPropuestos;
 
+import java.util.Scanner;
+
 public class EJ_esBisiesto {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int año;
+        System.out.print("Ingrese un año para verificar si es bisiesto: ");
+        año = sc.nextInt();
+        if (año % 4 == 0 && (año % 100 != 0 || año % 400 == 0)) {
+            System.out.println("El año " + año + " es bisiesto.");
+        } else {
+            System.out.println("El año " + año + " no es bisiesto.");
+        }
+    }
 }
