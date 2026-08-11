7f7a5faca2b7e817036dcd4760b0bd9f1c712880
Ian <ianfrancomiguez@gmail.com>
2026-08-11 11:57:57 -0300

    Ejercicio notas >= 3.0

---
+++ src/EjerciciosPropuestos_do_while/EJ_notas_mayores_a_3.java
@@ -0,0 +1,23 @@
+package EjerciciosPropuestos_do_while;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class EJ_notas_mayores_a_3 {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+        double nota;
+        int cont = 0;
+        int i = 0;
+        do {
+            System.out.print("Ingrese una nota: ");
+            nota = sc.nextDouble();
+            if (nota >= 3.0){
+                cont++;
+            }
+            i++;
+        } while (i<5);
+        System.out.println("Notas mayores o iguales a 3.0: "+cont);
+    }
+}
