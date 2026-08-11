d2920f6e21d9342028a59d9d57625a72a9917b29
Ian <ianfrancomiguez@gmail.com>
2026-08-10 21:18:49 -0300

    Ejercicio calcular suma 1+1/2+...+1/n

--- src/EjerciciosPropuestos_while/EJ_calcularSuma.java
+++ src/EjerciciosPropuestos_while/EJ_calcularSuma.java
@@ -1,4 +1,23 @@
 package EjerciciosPropuestos_while;
 
+import java.util.Scanner;
+
 public class EJ_calcularSuma {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        int i = 1;
+        double suma = 0;
+        System.out.print("Ingrese un número entero positivo: ");
+        n = sc.nextInt();
+        while (n <= 0) {
+            System.out.print("Número inválido. Ingrese un número entero positivo: ");
+            n = sc.nextInt();
+        }
+        while (i <= n) {
+            suma += (double) 1/i;
+            i++;
+        }
+        System.out.println("La suma de los números ingresados es: " + suma);
+    }
 }
