1af71fcd61fd68a67f886eafecc463f1c40c6be9
Ian <ianfrancomiguez@gmail.com>
2026-08-25 19:46:57 -0300

    Ejercicio Elementos Comunes en dos Arrays

---
+++ src/Cap5_arrays/EjElementosComunes.java
@@ -0,0 +1,17 @@
+package Cap5_arrays;
+
+public class EjElementosComunes {
+    static void main() {
+        int[] array1 = {1, 2, 3, 4, 5, 8};
+        int[] array2 = {4, 5, 6, 7, 8};
+
+        System.out.print("Elementos comunes: ");
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array2.length; j++) {
+                if (array1[i] == array2[j]) {
+                    System.out.print(array1[i] + " ");
+                }
+            }
+        }
+    }
+}
